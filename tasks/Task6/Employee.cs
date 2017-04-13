eb4f7c49f38c13b620bf3e1d2dc76ce5651af404
seekaddo <dennis_addo@aol.com>
2017-04-13 16:37:27 +0200

    task6.1 with Rx

---
+++ tasks/Task6/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task6
+{
+    public class Employee
+    {
+        public string Name { get; }
+        public string EmployeeId { get; }
+        public string JobType { get; }
+        public string Company { get; }
+        public string Location { get; }
+
+        public Employee(string name, string employeeId, string jobType, string company, string location)
+        {
+            if(string.IsNullOrEmpty(name)) throw new ArgumentException("Name can't be null {0}",nameof(name));
+            if(jobType != null &&string.IsNullOrEmpty(employeeId)) throw new ArgumentException("employeeId can't be null {0}",nameof(employeeId));
+            if(string.IsNullOrEmpty(name)) throw new ArgumentException("Name can't be null {0}",nameof(name));
+            if(string.IsNullOrEmpty(location)) throw new ArgumentException("I hope you are not living on the strights {0}",nameof(location));
+
+            Name = name;
+            EmployeeId = employeeId;
+            JobType = jobType;
+            Company = company;
+            Location = location;
+        }
+
+
+        public bool Isemployed()
+        {
+            return this.JobType != null;
+        }
+    }
+
+
+}
---
+++ tasks/Task6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Task6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var employeeList = new List<Employee>()
+                               {
+                                   new Employee("Larry Johns","fh2015","System Administrator","GM Motors","Germany"),
+                                   new Employee("Morris Hasdoc","wks2015","Publisher","RTL DE","Germany"),
+                                   new Employee("Loskav Sissi","bmw200s","C/C++ Developer","BMW","Russia"),
+                                   new Employee("James Braokigi","ksl0105","Techer","Wiener Kindergarten","Austria"),
+                                   new Employee("Marry Adicos","ms4627xxx","Software Architect","OMV","Austria"),
+                                   new Employee("Kannus Gerrad","as2003","IT Specialist","Kaspersky Lab","USA"),
+                                   new Employee("Adam Borrows",null,null,"Online","Germany")
+                               };
+
+            //<summary>
+            //initialising Subject and subscriber
+            //processing data and converting it to obersable
+            //</summary>
+            var developers = new Subject<Employee>();
+            Console.WriteLine("<----------List of Software Developers---------->");
+
+            developers.Subscribe(p => Console.WriteLine("Name: {0,-5}  Location:{1}",p.Name,p.Location));
+
+            var re = from p in employeeList
+                     where p.JobType != null && p.JobType.Contains("Developer")
+                     select p;
+
+            foreach (var employee in re)
+            {
+                developers.OnNext(employee);
+
+            }
+
+
+
+            var germanyemployees = employeeList.Where(e => e.Isemployed() && e.Location.Contains("Germany")).ToObservable();
+            Console.WriteLine();
+            Console.WriteLine("<----------List of Employees in Germany---------->");
+
+            germanyemployees.Subscribe(
+                x => Console.WriteLine("Name: {0,-5}     JobType: {1,-5}", x.Name,x.JobType),
+                ex => Console.WriteLine("OnError: {0}",ex.Message),
+                ()=> Console.WriteLine("OnComplete")
+            );
+
+
+
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
